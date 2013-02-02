30150f766c0cc2c463447095fcc7d61ab5391305
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2013-02-02 11:40:41 +0000

    Refactored metadata handling. Moved the common parts of GExiv2 and pyexiv2 metadata handlers into the metadata module, reducing the library specific parts to a simple handler class.

--- code/photini/metadata.py
+++ code/photini/metadata.py
@@ -16,7 +16,197 @@
 ##  along with this program.  If not, see
 ##  <http://www.gnu.org/licenses/>.
 
+import datetime
+import fractions
+import os
+
+from PyQt4 import QtCore
+
 try:
-    from metadata_gexiv2 import *
+    from metadata_gexiv2 import MetadataHandler
 except ImportError:
-    from metadata_pyexiv2 import *
+    from metadata_pyexiv2 import MetadataHandler
+from version import version, release
+
+class GPSvalue(object):
+    def __init__(self, degrees=0.0, latitude=True):
+        self.degrees = degrees
+        self.latitude = latitude
+
+    def from_xmp_string(self, value):
+        degrees, residue = value.split(',')
+        minutes = residue[:-1]
+        direction = residue[-1]
+        self.degrees = float(degrees) + (float(minutes) / 60.0)
+        if direction in ('S', 'W'):
+            self.degrees = -self.degrees
+        self.latitude = direction in ('S', 'N')
+        return self
+
+    def to_xmp_string(self):
+        if self.degrees >= 0.0:
+            ref = ('E', 'N')[self.latitude]
+            value = self.degrees
+        else:
+            ref = ('W', 'S')[self.latitude]
+            value = -self.degrees
+        degrees = int(value)
+        minutes = (value - degrees) * 60.0
+        return '%d,%.13f%s' % (degrees, minutes, ref)
+
+    def from_exif_string(self, value, direction):
+        parts = map(fractions.Fraction, value.split())
+        self.degrees = float(parts[0])
+        if len(parts) > 1:
+            self.degrees += float(parts[1]) / 60.0
+        if len(parts) > 2:
+            self.degrees += float(parts[2]) / 3600.0
+        if direction in ('S', 'W'):
+            self.degrees = -self.degrees
+        self.latitude = direction in ('S', 'N')
+        return self
+
+    def to_exif_string(self):
+        if self.degrees >= 0.0:
+            ref = ('E', 'N')[self.latitude]
+            value = self.degrees
+        else:
+            ref = ('W', 'S')[self.latitude]
+            value = -self.degrees
+        degrees = int(value)
+        value = (value - degrees) * 60.0
+        minutes = int(value)
+        seconds = (value - minutes) * 60.0
+        degrees = fractions.Fraction(degrees).limit_denominator(1000000)
+        minutes = fractions.Fraction(minutes).limit_denominator(1000000)
+        seconds = fractions.Fraction(seconds).limit_denominator(1000000)
+        return '%d/%d %d/%d %d/%d' % (
+            degrees.numerator, degrees.denominator,
+            minutes.numerator, minutes.denominator,
+            seconds.numerator, seconds.denominator), ref
+
+class Metadata(QtCore.QObject):
+    _keys = {
+        'date_digitised' : (('Exif.Photo.DateTimeDigitized',     True),),
+        'date_modified'  : (('Exif.Image.DateTime',              True),),
+        'date_taken'     : (('Exif.Photo.DateTimeOriginal',      True),
+                            ('Exif.Image.DateTimeOriginal',      True),),
+        'title'          : (('Xmp.dc.title',                     True),
+                            ('Iptc.Application2.ObjectName',     True),
+                            ('Exif.Image.ImageDescription',      True),),
+        'creator'        : (('Xmp.dc.creator',                   True),
+                            ('Xmp.tiff.Artist',                  False),
+                            ('Iptc.Application2.Byline',         True),
+                            ('Exif.Image.Artist',                True),),
+        'description'    : (('Xmp.dc.description',               True),
+                            ('Iptc.Application2.Caption',        True),),
+        'keywords'       : (('Xmp.dc.subject',                   True),
+                            ('Iptc.Application2.Keywords',       True),),
+        'copyright'      : (('Xmp.dc.rights',                    True),
+                            ('Xmp.tiff.Copyright',               False),
+                            ('Iptc.Application2.Copyright',      True),
+                            ('Exif.Image.Copyright',             True),),
+        'latitude'       : (('Exif.GPSInfo.GPSLatitude',         True),
+                            ('Xmp.exif.GPSLatitude',             True),),
+        'longitude'      : (('Exif.GPSInfo.GPSLongitude',        True),
+                            ('Xmp.exif.GPSLongitude',            True),),
+        'orientation'    : (('Exif.Image.Orientation',           True),),
+        'soft_full'      : (('Exif.Image.ProcessingSoftware',    True),),
+        'soft_name'      : (('Iptc.Application2.Program',        True),),
+        'soft_vsn'       : (('Iptc.Application2.ProgramVersion', True),),
+        }
+    _list_items = ('keywords',)
+    def __init__(self, path, parent=None):
+        QtCore.QObject.__init__(self, parent)
+        self._mh = MetadataHandler(path)
+        self._new = False
+
+    def save(self):
+        if not self._new:
+            return
+        self.set_item('soft_full', 'Photini editor v%s_%s' % (version, release))
+        self.set_item('soft_name', 'Photini editor')
+        self.set_item('soft_vsn', '%s_%s' % (version, release))
+        self._mh.save()
+        self._set_status(False)
+
+    def has_GPS(self):
+        return (('Xmp.exif.GPSLatitude' in self._mh.get_xmp_tags()) or
+                ('Exif.GPSInfo.GPSLatitude' in self._mh.get_exif_tags()))
+
+    def get_item(self, name):
+        for key, required in self._keys[name]:
+            family, group, tag = key.split('.')
+            if key in self._mh.get_xmp_tags():
+                if tag.startswith('GPS'):
+                    return GPSvalue().from_xmp_string(
+                        self._mh.get_xmp_tag_string(key))
+                return u'; '.join(self._mh.get_xmp_tag_multiple(key))
+            if key in self._mh.get_iptc_tags():
+                return u'; '.join(self._mh.get_iptc_tag_multiple(key))
+            if key in self._mh.get_exif_tags():
+                if tag.startswith('DateTime'):
+                    return datetime.datetime.strptime(
+                        self._mh.get_exif_tag_string(key), '%Y:%m:%d %H:%M:%S')
+                if tag == 'Orientation':
+                    return int(self._mh.get_exif_tag_string(key))
+                if group == 'GPSInfo':
+                    return GPSvalue().from_exif_string(
+                        self._mh.get_exif_tag_string(key),
+                        self._mh.get_exif_tag_string('%sRef' % key))
+                return unicode(self._mh.get_exif_tag_string(key), 'iso8859_1')
+        return None
+
+    def set_item(self, name, value):
+        if value == self.get_item(name):
+            return
+        if name in self._list_items:
+            value = map(lambda x: x.strip(), value.split(';'))
+            for i in reversed(range(len(value))):
+                if not value[i]:
+                    del value[i]
+        elif isinstance(value, (str, unicode)):
+            value = [value.strip()]
+        if not value:
+            self.del_item(name)
+            return
+        for key, required in self._keys[name]:
+            if required or key in self._mh.get_tags():
+                family, group, tag = key.split('.')
+                if family == 'Xmp':
+                    if isinstance(value, GPSvalue):
+                        self._mh.set_xmp_tag_string(key, value.to_xmp_string())
+                    else:
+                        self._mh.set_xmp_tag_multiple(key, value)
+                elif family == 'Iptc':
+                    self._mh.set_iptc_tag_multiple(key, value)
+                elif family == 'Exif':
+                    if isinstance(value, GPSvalue):
+                        string, ref = value.to_exif_string()
+                        self._mh.set_exif_tag_string(key, string)
+                        self._mh.set_exif_tag_string('%sRef' % key, ref)
+                    elif isinstance(value, datetime.datetime):
+                        self._mh.set_exif_tag_string(
+                            key, value.strftime('%Y:%m:%d %H:%M:%S'))
+                    elif isinstance(value, int):
+                        self._mh.set_exif_tag_long(key, value)
+                    else:
+                        self._mh.set_exif_tag_string(key, value[0])
+        self._set_status(True)
+
+    def del_item(self, name):
+        changed = False
+        for key, required in self._keys[name]:
+            if key in self._mh.get_tags():
+                self._mh.clear_tag(key)
+                changed = True
+        if changed:
+            self._set_status(True)
+
+    new_status = QtCore.pyqtSignal(bool)
+    def _set_status(self, status):
+        self._new = status
+        self.new_status.emit(self._new)
+
+    def changed(self):
+        return self._new
--- code/photini/metadata_pyexiv2.py
+++ code/photini/metadata_pyexiv2.py
@@ -16,206 +16,66 @@
 ##  along with this program.  If not, see
 ##  <http://www.gnu.org/licenses/>.
 
-import datetime
-import fractions
-import os
-
 import pyexiv2
-from PyQt4 import QtCore
 
-from version import version, release
-
-class GPSvalue(object):
-    def __init__(self, degrees=0.0, latitude=True):
-        self.degrees = degrees
-        self.latitude = latitude
-
-    def from_xmp_string(self, value):
-        degrees, residue = value.split(',')
-        minutes = residue[:-1]
-        direction = residue[-1]
-        self.degrees = float(degrees) + (float(minutes) / 60.0)
-        if direction in ('S', 'W'):
-            self.degrees = -self.degrees
-        self.latitude = direction in ('S', 'N')
-        return self
-
-    def to_xmp_string(self):
-        if self.degrees >= 0.0:
-            ref = ('E', 'N')[self.latitude]
-            value = self.degrees
-        else:
-            ref = ('W', 'S')[self.latitude]
-            value = -self.degrees
-        degrees = int(value)
-        minutes = (value - degrees) * 60.0
-        return '%d,%.13f%s' % (degrees, minutes, ref)
-
-    def from_exif_string(self, value, direction):
-        parts = map(fractions.Fraction, value.split())
-        self.degrees = float(parts[0])
-        if len(parts) > 1:
-            self.degrees += float(parts[1]) / 60.0
-        if len(parts) > 2:
-            self.degrees += float(parts[2]) / 3600.0
-        if direction in ('S', 'W'):
-            self.degrees = -self.degrees
-        self.latitude = direction in ('S', 'N')
-        return self
-
-    def to_exif_string(self):
-        if self.degrees >= 0.0:
-            ref = ('E', 'N')[self.latitude]
-            value = self.degrees
-        else:
-            ref = ('W', 'S')[self.latitude]
-            value = -self.degrees
-        degrees = int(value)
-        value = (value - degrees) * 60.0
-        minutes = int(value)
-        seconds = (value - minutes) * 60.0
-        degrees = fractions.Fraction(degrees).limit_denominator(1000000)
-        minutes = fractions.Fraction(minutes).limit_denominator(1000000)
-        seconds = fractions.Fraction(seconds).limit_denominator(1000000)
-        return '%d/%d %d/%d %d/%d' % (
-            degrees.numerator, degrees.denominator,
-            minutes.numerator, minutes.denominator,
-            seconds.numerator, seconds.denominator), ref
-
-class Metadata(QtCore.QObject):
-    _keys = {
-        'date_digitised' : (('Exif.Photo.DateTimeDigitized',     True),),
-        'date_modified'  : (('Exif.Image.DateTime',              True),),
-        'date_taken'     : (('Exif.Photo.DateTimeOriginal',      True),
-                            ('Exif.Image.DateTimeOriginal',      True),),
-        'title'          : (('Xmp.dc.title',                     True),
-                            ('Iptc.Application2.ObjectName',     True),
-                            ('Exif.Image.ImageDescription',      True),),
-        'creator'        : (('Xmp.dc.creator',                   True),
-                            ('Xmp.tiff.Artist',                  False),
-                            ('Iptc.Application2.Byline',         True),
-                            ('Exif.Image.Artist',                True),),
-        'description'    : (('Xmp.dc.description',               True),
-                            ('Iptc.Application2.Caption',        True),),
-        'keywords'       : (('Xmp.dc.subject',                   True),
-                            ('Iptc.Application2.Keywords',       True),),
-        'copyright'      : (('Xmp.dc.rights',                    True),
-                            ('Xmp.tiff.Copyright',               False),
-                            ('Iptc.Application2.Copyright',      True),
-                            ('Exif.Image.Copyright',             True),),
-        'latitude'       : (('Exif.GPSInfo.GPSLatitude',         True),
-                            ('Xmp.exif.GPSLatitude',             True),),
-        'longitude'      : (('Exif.GPSInfo.GPSLongitude',        True),
-                            ('Xmp.exif.GPSLongitude',            True),),
-        'orientation'    : (('Exif.Image.Orientation',           True),),
-        'soft_full'      : (('Exif.Image.ProcessingSoftware',    True),),
-        'soft_name'      : (('Iptc.Application2.Program',        True),),
-        'soft_vsn'       : (('Iptc.Application2.ProgramVersion', True),),
-        }
-    _list_items = ('keywords',)
-    def __init__(self, path, parent=None):
-        QtCore.QObject.__init__(self, parent)
+class MetadataHandler(object):
+    def __init__(self, path):
         self._md = pyexiv2.ImageMetadata(path)
         self._md.read()
-        self._new = False
 
     def save(self):
-        if not self._new:
-            return
-        self.set_item('soft_full', 'Photini editor v%s_%s' % (version, release))
-        self.set_item('soft_name', 'Photini editor')
-        self.set_item('soft_vsn', '%s_%s' % (version, release))
-        self._md.write()
-        self._set_status(False)
+        return self._md.write()
 
-    def has_GPS(self):
-        return (('Xmp.exif.GPSLatitude' in self._md.xmp_keys) or
-                ('Exif.GPSInfo.GPSLatitude' in self._md.exif_keys))
+    def get_tags(self):
+        return self._md.exif_keys + self._md.iptc_keys + self._md.xmp_keys
 
-    def get_item(self, name):
-        for key, required in self._keys[name]:
-            family, group, tag = key.split('.')
-            if key in self._md.xmp_keys:
-                item = self._md[key]
-                if item.type.split()[0] in ('bag', 'seq'):
-                    return '; '.join(item.value)
-                if item.type == 'Lang Alt':
-                    return '; '.join(item.value.values())
-                if item.type == 'GPSCoordinate':
-                    return GPSvalue().from_xmp_string(item.raw_value)
-                print key, item.type, item.value
-                return item.value
-            if key in self._md.iptc_keys:
-                return '; '.join(map(lambda x: unicode(x, 'iso8859_1'),
-                                     self._md[key].value))
-            if key in self._md.exif_keys:
-                value = self._md[key].value
-                if isinstance(value, (datetime.datetime, (int, long))):
-                    return value
-                elif group == 'GPSInfo':
-                    return GPSvalue().from_exif_string(
-                        self._md[key].raw_value, self._md['%sRef' % key].value)
-                else:
-                    return unicode(value, 'iso8859_1')
-        return None
+    def get_exif_tags(self):
+        return self._md.exif_keys
 
-    def set_item(self, name, value):
-        if value == self.get_item(name):
-            return
-        if name in self._list_items:
-            value = map(lambda x: x.strip(), value.split(';'))
-            for i in reversed(range(len(value))):
-                if not value[i]:
-                    del value[i]
-        elif isinstance(value, (str, unicode)):
-            value = [value.strip()]
-        if not value:
-            self.del_item(name)
-            return
-        for key, required in self._keys[name]:
-            if required or key in (self._md.xmp_keys +
-                                   self._md.iptc_keys +
-                                   self._md.exif_keys):
-                family, group, tag = key.split('.')
-                if family == 'Xmp':
-                    new_tag = pyexiv2.XmpTag(key)
-                    if new_tag.type.split()[0] in ('bag', 'seq'):
-                        new_tag = pyexiv2.XmpTag(key, value)
-                    elif new_tag.type == 'Lang Alt':
-                        new_tag = pyexiv2.XmpTag(key, {'x-default': value[0]})
-                    elif new_tag.type == 'GPSCoordinate':
-                        new_tag.raw_value = value.to_xmp_string()
-                    else:
-                        raise KeyError("Unknown type %s" % new_tag.type)
-                elif family == 'Iptc':
-                    new_tag = pyexiv2.IptcTag(key, value)
-                elif family == 'Exif':
-                    new_tag = pyexiv2.ExifTag(key)
-                    if group == 'GPSInfo':
-                        new_tag.raw_value, ref = value.to_exif_string()
-                        self._md['%sRef' % key] = ref
-                    elif isinstance(value, (datetime.datetime, int, long)):
-                        new_tag = pyexiv2.ExifTag(key, value)
-                    else:
-                        new_tag = pyexiv2.ExifTag(key, value[0])
-                self._md[key] = new_tag
-        self._set_status(True)
+    def get_iptc_tags(self):
+        return self._md.iptc_keys
 
-    def del_item(self, name):
-        changed = False
-        for key, required in self._keys[name]:
-            if key in (self._md.xmp_keys +
-                       self._md.iptc_keys +
-                       self._md.exif_keys):
-                del self._md[key]
-                changed = True
-        if changed:
-            self._set_status(True)
+    def get_xmp_tags(self):
+        return self._md.xmp_keys
 
-    new_status = QtCore.pyqtSignal(bool)
-    def _set_status(self, status):
-        self._new = status
-        self.new_status.emit(self._new)
+    def get_exif_tag_string(self, tag):
+        return self._md[tag].raw_value
 
-    def changed(self):
-        return self._new
+    def get_iptc_tag_multiple(self, tag):
+        return self._md[tag].value
+
+    def get_xmp_tag_string(self, tag):
+        return self._md[tag].raw_value
+
+    def get_xmp_tag_multiple(self, tag):
+        item = self._md[tag]
+        if item.type == 'Lang Alt':
+            return item.value.values()
+        return item.value
+
+    def set_exif_tag_string(self, tag, value):
+        new_tag = pyexiv2.ExifTag(tag)
+        new_tag.raw_value = value
+        self._md[tag] = new_tag
+
+    def set_exif_tag_long(self, tag, value):
+        self._md[tag] = pyexiv2.ExifTag(tag, value)
+
+    def set_iptc_tag_multiple(self, tag, value):
+        self._md[tag] = pyexiv2.IptcTag(tag, value)
+
+    def set_xmp_tag_string(self, tag, value):
+        new_tag = pyexiv2.XmpTag(tag)
+        new_tag.raw_value = value
+        self._md[tag] = new_tag
+
+    def set_xmp_tag_multiple(self, tag, value):
+        new_tag = pyexiv2.XmpTag(tag)
+        if new_tag.type == 'Lang Alt':
+            new_tag = pyexiv2.XmpTag(tag, {'x-default': value[0]})
+        else:
+            new_tag = pyexiv2.XmpTag(tag, value)
+        self._md[tag] = new_tag
+
+    def clear_tag(self, tag):
+        del self._md[tag]
