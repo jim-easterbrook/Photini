d09984e3ccd82b2e51e72b8409564f7b280ed598
Joel Robinson <laconic75@gmail.com>
2014-06-24 14:39:53 -0500

    Fixed error parsing proxy settings.

--- src/photini/editor.py
+++ src/photini/editor.py
@@ -36,6 +36,7 @@ if sys.version_info[0] >= 3:
     from urllib.request import getproxies
 else:
     from urllib import getproxies
+from urlparse import urlparse 
 import webbrowser
 
 from PyQt4 import QtGui, QtCore
@@ -79,9 +80,9 @@ class MainWindow(QtGui.QMainWindow):
         # set network proxy
         proxies = getproxies()
         if 'http' in proxies:
-            scheme, host, port = proxies['http'].split(':')
+            parsed = urlparse(proxies['http'])
             QNetworkProxy.setApplicationProxy(
-                QNetworkProxy(QNetworkProxy.HttpProxy, host[2:], int(port)))
+                QNetworkProxy(QNetworkProxy.HttpProxy, parsed.hostname, parsed.port))
         # restore size
         size = self.width(), self.height()
         self.resize(*eval(
