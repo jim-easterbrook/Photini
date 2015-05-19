42bf6a8bfa0a6272bd1171ffa25e43b8b10e5e80
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-05-19 15:10:31 +0100

    Use a pre-scaled logo for documentation

--- src/doc/conf.py
+++ src/doc/conf.py
@@ -115,7 +115,7 @@ html_theme = 'default'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #html_logo = None
-html_logo = '../misc/icon_master.png'
+html_logo = '../photini/data/icon_200.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
--- src/photini/__init__.py
+++ src/photini/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
 
-__version__ = '15.05.0.dev212'
-_dev_no = '212'
-_commit = '91b4c75'
+__version__ = '15.05.1.dev213'
+_dev_no = '213'
+_commit = '5ce44dc'
