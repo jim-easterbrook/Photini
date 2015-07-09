a21262b7473b6b2bc555956d7dc60d8f969eb654
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-07-09 14:41:22 +0100

    Add common Spanish translation

--- src/photini/__init__.py
+++ src/photini/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import unicode_literals
 
-__version__ = '15.07.2.dev299'
-_dev_no = '299'
-_commit = '92a42e6'
+__version__ = '15.07.2.dev300'
+_dev_no = '300'
+_commit = '8092274'
