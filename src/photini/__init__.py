f69033c8cef90381e9b270705e546cacd01f39ea
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-08-26 14:28:15 +0100

    Update documentation to mention PyQt5

--- src/photini/__init__.py
+++ src/photini/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import unicode_literals
 
-__version__ = '15.08.0.dev321'
-_dev_no = '321'
-_commit = '253f5a2'
+__version__ = '15.08.0.dev322'
+_dev_no = '322'
+_commit = '27f0148'
