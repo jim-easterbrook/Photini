1ba44931bd4130496857c05fff55dc4c3aa5a414
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-09-16 11:29:15 +0100

    Only intercept stderr & stdout if they exist

--- src/photini/__init__.py
+++ src/photini/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import unicode_literals
 
-__version__ = '15.09.0.dev335'
-_dev_no = '335'
-_commit = 'f703503'
+__version__ = '15.09.0.dev336'
+_dev_no = '336'
+_commit = '41c76e8'
