6c6287b7bbe0020de16cd19c0b14aa29f40e6b18
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-09-22 07:46:18 +0100

    Updated change log

--- src/photini/__init__.py
+++ src/photini/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import unicode_literals
 
-__version__ = '15.09.1.dev341'
-_dev_no = '341'
-_commit = '4ecc775'
+__version__ = '15.09.1.dev342'
+_dev_no = '342'
+_commit = '2f69ef7'
