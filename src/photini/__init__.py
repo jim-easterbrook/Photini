ce05633338949eac5115644333148b18c1ccee77
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-10-28 11:31:04 +0000

    New screen grabs

--- src/photini/__init__.py
+++ src/photini/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import unicode_literals
 
-__version__ = '15.10.0.dev404'
-_dev_no = '404'
-_commit = '18ba3e1'
+__version__ = '15.10.0.dev405'
+_dev_no = '405'
+_commit = 'a216f60'
