1768a8c9a13656d51a97a06453d2e447789ec5b2
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-10-29 10:22:50 +0000

    Revert XMP tag preferences.
    As it says in the MWG guidelines: In the case the file format does
    support Exif natively, Exif and TIFF device properties (e.g.
    XResolution, YResolution, WhitePoint, etc.) SHOULD NOT be duplicated in
    the XMP exif: and tiff: namespaces.

--- src/photini/__init__.py
+++ src/photini/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import unicode_literals
 
-__version__ = '15.10.0.dev411'
-_dev_no = '411'
-_commit = 'c1468dc'
+__version__ = '15.10.0.dev412'
+_dev_no = '412'
+_commit = '3d89bd0'
