f5ae647564c171996823d74c7b5eea4a0a0e53bb
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-11-05 08:03:09 +0000

    Add alpha channel to screen grab images

--- src/photini/__init__.py
+++ src/photini/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import unicode_literals
 
-__version__ = '15.11.0.dev446'
-_dev_no = '446'
-_commit = '5a70126'
+__version__ = '15.11.0.dev447'
+_dev_no = '447'
+_commit = '22a471c'
