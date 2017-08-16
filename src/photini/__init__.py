jim-easterbrook/Photini

commit 669297ff5cf0fc21c368301dad907fe2fa9f96b6

Add option to preserve file timestamps

src/photini/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import unicode_literals
 
 __version__ = '2017.8.1'
-build = '886 (12e900f)'
+build = '887 (ee5dc6b)'

src/photini/editsettings.py
@@ -97,6 +97,13 @@ class EditSettings(QtWidgets.QDialog):
         self.write_if.setChecked(if_mode)
         self.write_if.clicked.connect(self.new_write_if)
         panel.layout().addRow(self.tr('Write to image'), self.write_if)
+        # preserve file timestamps
+        keep_time = eval(
+            self.config_store.get('files', 'preserve_timestamps', 'False'))
+        self.keep_time = QtWidgets.QCheckBox()
+        self.keep_time.setChecked(keep_time)
+        panel.layout().addRow(
+            self.tr('Preserve file timestamps'), self.keep_time)
         # add panel to scroll area after its size is known
         scroll_area.setWidget(panel)
 
@@ -134,4 +141,6 @@ class EditSettings(QtWidgets.QDialog):
             sc_mode = 'delete'
         self.config_store.set('files', 'sidecar', sc_mode)
         self.config_store.set('files', 'image', str(self.write_if.isChecked()))
+        self.config_store.set(
+            'files', 'preserve_timestamps', str(self.keep_time.isChecked()))
         return self.accept()

src/photini/imagelist.py
@@ -56,6 +56,8 @@ class Image(QtWidgets.QFrame):
         self.thumb_size = thumb_size
         # read metadata
         self.metadata = Metadata(self.path, new_status=self.show_status)
+        self.file_times = (os.path.getatime(self.path),
+                           os.path.getmtime(self.path))
         # set file type
         self.file_type = self.metadata.get_mime_type()
         if not self.file_type:
@@ -627,13 +629,17 @@ class ImageList(QtWidgets.QWidget):
         sc_mode = self.app.config_store.get('files', 'sidecar', 'auto')
         force_iptc = eval(
             self.app.config_store.get('files', 'force_iptc', 'False'))
+        keep_time = eval(
+            self.app.config_store.get('files', 'preserve_timestamps', 'False'))
+        if not images:
+            images = self.images
         with Busy():
-            if images:
-                for image in images:
-                    image.metadata.save(if_mode, sc_mode, force_iptc)
-            else:
-                for image in self.images:
-                    image.metadata.save(if_mode, sc_mode, force_iptc)
+            for image in images:
+                if keep_time:
+                    file_times = image.file_times
+                else:
+                    file_times = None
+                image.metadata.save(if_mode, sc_mode, force_iptc, file_times)
         unsaved = False
         for image in self.images:
             if image.metadata.changed():

src/photini/metadata.py
@@ -987,9 +987,11 @@ class MetadataHandler(GExiv2.Metadata):
             tag = tag[0]
         return GExiv2.Metadata.is_xmp_tag(tag)
 
-    def save(self):
+    def save(self, file_times):
         try:
             self.save_file(self._path)
+            if file_times:
+                os.utime(self._path, file_times)
         except Exception as ex:
             self._logger.exception(ex)
             return False
@@ -1243,7 +1245,7 @@ class Metadata(object):
         if self._sc and self._if:
             self._sc.copy(self._if)
 
-    def save(self, if_mode, sc_mode, force_iptc):
+    def save(self, if_mode, sc_mode, force_iptc, file_times):
         if not self._unsaved:
             return
         self.software = 'Photini editor v' + __version__
@@ -1267,7 +1269,7 @@ class Metadata(object):
             self._if.copy(self._sc)
         OK = False
         if self._if and if_mode:
-            OK = self._if.save()
+            OK = self._if.save(file_times)
             if OK:
                 # check that data really was saved
                 saved_tags = MetadataHandler(self._path).get_all_tags()
@@ -1283,7 +1285,7 @@ class Metadata(object):
         if sc_mode == 'always' and not self._sc:
             self.create_side_car()
         if self._sc:
-            OK = self._sc.save()
+            OK = self._sc.save(file_times)
         self._set_unsaved(not OK)
 
     # getters: use sidecar if tag is present, otherwise use image file

commit 5bd7d66b7fd503cd580567f69a9099b80ed01d10

Fix uploader bug introduced in commit 669297f

src/photini/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import unicode_literals
 
 __version__ = '2017.8.1'
-build = '891 (229b08b)'
+build = '892 (4b3b26d)'

src/photini/uploader.py
@@ -245,7 +245,7 @@ class PhotiniUploader(QtWidgets.QWidget):
         # copy metadata, forcing IPTC creation
         md = Metadata(path)
         md.copy(image.metadata)
-        md.save(True, 'none', True)
+        md.save(True, 'none', True, None)
 
     def convert_to_jpeg(self, image):
         im = QtGui.QImage(image.path)