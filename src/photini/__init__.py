06f1b1988db23a5cad98bbc6c16406a64a6c556d
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2019-04-29 09:30:34 +0100

    Ensure mapTypeControl is enabled on Google map

--- src/photini/__init__.py
+++ src/photini/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import unicode_literals
 
 __version__ = '2019.4.0'
-build = '1181 (3690f35)'
+build = '1182 (9a6e534)'
--- src/photini/data/googlemap/script.js
+++ src/photini/data/googlemap/script.js
@@ -1,6 +1,6 @@
 //  Photini - a simple photo metadata editor.
 //  http://github.com/jim-easterbrook/Photini
-//  Copyright (C) 2012-18  Jim Easterbrook  jim@jim-easterbrook.me.uk
+//  Copyright (C) 2012-19  Jim Easterbrook  jim@jim-easterbrook.me.uk
 //
 //  This program is free software: you can redistribute it and/or
 //  modify it under the terms of the GNU General Public License as
@@ -31,6 +31,7 @@ function loadMap()
         zoom: initData.zoom,
         maxZoom: 20,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: true,
         mapTypeControlOptions: {
             style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
             },
