jim-easterbrook/Photini

commit b373dc4452a5949d494f0dcf2e294fd414cdec65

Fix opening map links when using QtWebEngine

src/photini/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import unicode_literals
 
 __version__ = '2019.8.0'
-build = '1324 (e4dc687)'
+build = '1325 (53e2d96)'

src/photini/photinimap.py
@@ -49,6 +49,12 @@ class MapWebPage(QWebPage):
         def javaScriptConsoleMessage(self, msg, line, source):
             logger.error('%s line %d: %s', source, line, msg)
 
+    def acceptNavigationRequest(self, url, type_, isMainFrame):
+        if url.isLocalFile():
+            url.setScheme('http')
+        webbrowser.open_new(url.toString())
+        return False
+
 
 class MapWebEnginePage(MapWebPage):
     def __init__(self, call_handler, *args, **kwds):
@@ -57,12 +63,8 @@ class MapWebEnginePage(MapWebPage):
         self.setWebChannel(self.web_channel)
         self.web_channel.registerObject('python', call_handler)
 
-    def acceptNavigationRequest(self, url, type_, isMainFrame):
-        webbrowser.open_new(url.toString())
-        return False
-
     def createWindow(self, type_):
-        return QWebPage(self)
+        return MapWebPage(self)
 
     def do_java_script(self, command):
         self.runJavaScript(command)

commit 6834682aadeb95e913ceea9cb7c866d4c01740ff

Fix QtWebEngine map link opening properly
Commit b373dc4 caused crashes when using QtWebKit.

src/photini/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import unicode_literals
 
 __version__ = '2019.8.0'
-build = '1325 (53e2d96)'
+build = '1326 (b373dc4)'

src/photini/photinimap.py
@@ -49,22 +49,23 @@ class MapWebPage(QWebPage):
         def javaScriptConsoleMessage(self, msg, line, source):
             logger.error('%s line %d: %s', source, line, msg)
 
-    def acceptNavigationRequest(self, url, type_, isMainFrame):
-        if url.isLocalFile():
-            url.setScheme('http')
-        webbrowser.open_new(url.toString())
-        return False
-
 
 class MapWebEnginePage(MapWebPage):
     def __init__(self, call_handler, *args, **kwds):
         super(MapWebEnginePage, self).__init__(*args, **kwds)
         self.web_channel = QtWebChannel.QWebChannel(parent=self)
         self.setWebChannel(self.web_channel)
-        self.web_channel.registerObject('python', call_handler)
+        if call_handler:
+            self.web_channel.registerObject('python', call_handler)
+
+    def acceptNavigationRequest(self, url, type_, isMainFrame):
+        if url.isLocalFile():
+            url.setScheme('http')
+        webbrowser.open_new(url.toString())
+        return False
 
     def createWindow(self, type_):
-        return MapWebPage(self)
+        return MapWebEnginePage(None, parent=self)
 
     def do_java_script(self, command):
         self.runJavaScript(command)