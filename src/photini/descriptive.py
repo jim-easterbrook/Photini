0c22ece0e675357d9b115ecf4d80eb7471e02096
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2023-03-21 10:59:40 +0000

    Update descriptive tab widgets

--- src/photini/descriptive.py
+++ src/photini/descriptive.py
@@ -30,21 +30,18 @@ translate = QtCore.QCoreApplication.translate
 
 
 class RatingWidget(QtWidgets.QWidget):
-    new_value = QtSignal(str, object)
-
     def __init__(self, key, *arg, **kw):
         super(RatingWidget, self).__init__(*arg, **kw)
-        self._key = key
         self.multiple_values = multiple_values()
         self.setLayout(QtWidgets.QHBoxLayout())
         self.layout().setContentsMargins(0, 0, 0, 0)
         # slider
-        self.slider = Slider(Qt.Orientation.Horizontal)
+        self.slider = Slider(key)
+        self.slider.setOrientation(Qt.Orientation.Horizontal)
         self.slider.setFixedWidth(width_for_text(self.slider, 'x' * 25))
         self.slider.setRange(-2, 5)
         self.slider.setPageStep(1)
         self.slider.valueChanged.connect(self.set_display)
-        self.slider.editing_finished.connect(self._new_value)
         self.layout().addWidget(self.slider)
         # display
         self.display = QtWidgets.QLineEdit()
@@ -59,11 +56,9 @@ class RatingWidget(QtWidgets.QWidget):
         self.layout().addWidget(self.display)
         # adopt child methods/signals
         self.is_multiple = self.slider.is_multiple
-
-    @QtSlot()
-    @catch_all
-    def _new_value(self):
-        self.new_value.emit(self._key, self.get_value())
+        self.new_value_dict = self.slider.new_value_dict
+        # over-ride child methods
+        self.slider.get_value = self.get_value
 
     @QtSlot(int)
     @catch_all
@@ -127,7 +122,7 @@ class KeywordsEditor(QtWidgets.QWidget):
         self.set_value = self.edit.set_value
         self.set_multiple = self.edit.set_multiple
         self.is_multiple = self.edit.is_multiple
-        self.new_value = self.edit.new_value
+        self.new_value_dict = self.edit.new_value_dict
 
     def update_favourites(self):
         self.favourites.clear()
@@ -174,7 +169,7 @@ class KeywordsEditor(QtWidgets.QWidget):
         if current_value:
             new_value = current_value + '; ' + new_value
         self.set_value(new_value)
-        self.new_value.emit(self.edit._key, new_value)
+        self.edit.emit_dict()
 
 
 class TabWidget(QtWidgets.QScrollArea):
@@ -199,7 +194,7 @@ class TabWidget(QtWidgets.QScrollArea):
         self.widgets['title'].setToolTip('<p>{}</p>'.format(translate(
             'DescriptiveTab', 'Enter a short verbal and human readable name'
             ' for the image, this may be the file name.')))
-        self.widgets['title'].new_value.connect(self.new_value)
+        self.widgets['title'].new_value_dict.connect(self.new_value)
         layout.addRow(translate('DescriptiveTab', 'Title / Object Name'),
                       self.widgets['title'])
         # headline
@@ -210,7 +205,7 @@ class TabWidget(QtWidgets.QScrollArea):
         self.widgets['headline'].setToolTip('<p>{}</p>'.format(translate(
             'DescriptiveTab', 'Enter a brief publishable synopsis or summary'
             ' of the contents of the image.')))
-        self.widgets['headline'].new_value.connect(self.new_value)
+        self.widgets['headline'].new_value_dict.connect(self.new_value)
         layout.addRow(translate('DescriptiveTab', 'Headline'),
                       self.widgets['headline'])
         # description
@@ -222,7 +217,7 @@ class TabWidget(QtWidgets.QScrollArea):
             ' and why of what is happening in this image, this might include'
             ' names of people, and/or their role in the action that is taking'
             ' place within the image.')))
-        self.widgets['description'].new_value.connect(self.new_value)
+        self.widgets['description'].new_value_dict.connect(self.new_value)
         layout.addRow(translate('DescriptiveTab', 'Description / Caption'),
                       self.widgets['description'])
         # alt text
@@ -233,7 +228,7 @@ class TabWidget(QtWidgets.QScrollArea):
             'DescriptiveTab', 'Enter text describing the appearance of the'
             ' image from a visual perspective, focusing on details that are'
             ' relevant to the purpose and meaning of the image.')))
-        self.widgets['alt_text'].new_value.connect(self.new_value)
+        self.widgets['alt_text'].new_value_dict.connect(self.new_value)
         layout.addRow(
             Label(translate('DescriptiveTab', 'Alt Text (Accessibility)'),
                   lines=2, layout=layout), self.widgets['alt_text'])
@@ -247,7 +242,7 @@ class TabWidget(QtWidgets.QScrollArea):
             ' This property does not have a character limitation and is not'
             ' required if the Alt Text (Accessibility) field sufficiently'
             ' describes the image..')))
-        self.widgets['alt_text_ext'].new_value.connect(self.new_value)
+        self.widgets['alt_text_ext'].new_value_dict.connect(self.new_value)
         layout.addRow(
             Label(translate('DescriptiveTab',
                             'Extended Description (Accessibility)'),
@@ -260,13 +255,13 @@ class TabWidget(QtWidgets.QScrollArea):
             'DescriptiveTab', 'Enter any number of keywords, terms or phrases'
             ' used to express the subject matter in the image.'
             ' Separate them with ";" characters.')))
-        self.widgets['keywords'].new_value.connect(self.new_value)
+        self.widgets['keywords'].new_value_dict.connect(self.new_value)
         layout.addRow(translate('DescriptiveTab', 'Keywords'),
                       self.widgets['keywords'])
         self.app.image_list.image_list_changed.connect(self.image_list_changed)
         # rating
         self.widgets['rating'] = RatingWidget('rating')
-        self.widgets['rating'].new_value.connect(self.new_value)
+        self.widgets['rating'].new_value_dict.connect(self.new_value)
         layout.addRow(translate('DescriptiveTab', 'Rating'),
                       self.widgets['rating'])
         # disable until an image is selected
@@ -284,9 +279,10 @@ class TabWidget(QtWidgets.QScrollArea):
         self.widgets['keywords'].update_league_table(
             self.app.image_list.get_images())
 
-    @QtSlot(str, object)
+    @QtSlot(dict)
     @catch_all
-    def new_value(self, key, value):
+    def new_value(self, value):
+        key, value = list(value.items())[0]
         images = self.app.image_list.get_selected_images()
         for image in images:
             setattr(image.metadata, key, value)
