c684318b752955816ae8317a05be0cba1f98e4f8
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2023-03-27 10:15:18 +0100

    Update translation sources

--- src/lang/en/photini.ts
+++ src/lang/en/photini.ts
@@ -44,6 +44,10 @@
         <translation></translation>
     </message>
     <message>
+        <source>Enter a full name of the location.</source>
+        <translation></translation>
+    </message>
+    <message>
         <source>Enter the name of the sublocation.</source>
         <translation></translation>
     </message>
@@ -68,6 +72,18 @@
         <translation></translation>
     </message>
     <message>
+        <source>Enter globally unique identifier(s) of the location. Separate them with &quot;;&quot; characters.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Altitude of the location in metres.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation></translation>
+    </message>
+    <message>
         <source>Street</source>
         <translation></translation>
     </message>
@@ -85,6 +101,14 @@
     </message>
     <message>
         <source>Region</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Location ID</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Altitude</source>
         <translation></translation>
     </message>
 </context>
@@ -289,6 +313,10 @@
     </message>
     <message>
         <source>Change album membership</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Replace image region notes</source>
         <translation></translation>
     </message>
     <message>
@@ -739,6 +767,10 @@
         <translation></translation>
     </message>
     <message>
+        <source>Replace image region notes</source>
+        <translation></translation>
+    </message>
+    <message>
         <source>Title</source>
         <translation></translation>
     </message>
@@ -783,6 +815,10 @@
     <name>LatLongDisplay</name>
     <message>
         <source>Lat, long</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Latitude and longitude (in degrees) as two decimal numbers separated by a comma.</source>
         <translation></translation>
     </message>
 </context>
@@ -1025,15 +1061,7 @@
         <translation></translation>
     </message>
     <message>
-        <source>Enter the work email address(es) for the person that created this image, such as name@domain.com.</source>
-        <translation></translation>
-    </message>
-    <message>
         <source>Email(s)</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Enter the work Web URL(s) for the person that created this image, such as http://www.domain.com/.</source>
         <translation></translation>
     </message>
     <message>
@@ -1041,19 +1069,7 @@
         <translation></translation>
     </message>
     <message>
-        <source>Enter the work phone number(s) for the person that created this image, using the international format, such as +1 (123) 456789.</source>
-        <translation></translation>
-    </message>
-    <message>
         <source>Phone(s)</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Enter address for the person that created this image.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Address</source>
         <translation></translation>
     </message>
     <message>
@@ -1089,6 +1105,10 @@
         <translation></translation>
     </message>
     <message>
+        <source>Creator / Licensor Contact Information</source>
+        <translation></translation>
+    </message>
+    <message>
         <source>Use %Y to insert the year the photograph was taken.</source>
         <translation></translation>
     </message>
@@ -1105,15 +1125,35 @@
         <translation></translation>
     </message>
     <message>
-        <source>URL for a licensor web page. May facilitate licensing of the image.</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Licensor URL</source>
-        <translation></translation>
-    </message>
-    <message>
         <source>Rights</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Enter the work email address for the person that created this image, such as name@domain.com.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Enter the work Web URL for the person that created this image, such as http://www.domain.com/.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Enter the work phone number for the person that created this image, using the international format, such as +1 (123) 456789.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Enter address detail (e.g. flat number or room number) for the person that created this image.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Detail Address</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Enter street address for the person that created this image.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Street Address</source>
         <translation></translation>
     </message>
     <message>
@@ -1130,10 +1170,6 @@
     </message>
     <message>
         <source>Apply template</source>
-        <translation></translation>
-    </message>
-    <message>
-        <source>Creator Contact Information</source>
         <translation></translation>
     </message>
 </context>
@@ -1308,19 +1344,134 @@
     </message>
     <message>
         <source>Update remote</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Photini: alt text</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>Missing alt text.</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <source>File &quot;{file_name}&quot; does not have any &quot;alt text&quot; for accessibility. Would you like to upload it anyway?</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>RegionsTab</name>
+    <message>
+        <source>Delete vertex</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>New vertex</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Unreadable image format</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>pixel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>A pixel of a digital image setting an absolute value.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>relative</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Relative part of the size of an image along the x- or the y-axis.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Free-text name of the region. Should be unique among all Region Names of an image.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Identifier of the region. Must be unique among all Region Identifiers of an image. Does not have to be unique beyond the metadata of this image.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Identifier</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Unit used for measuring dimensions of the boundary of a region.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Boundary unit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Role of this region among all regions of this image or of other images. The value SHOULD be taken from a Controlled Vocabulary.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Role</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>The semantic type of what is shown inside the region. The value SHOULD be taken from a Controlled Vocabulary.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Content type</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Enter the names of people shown in this region. Separate multiple entries with &quot;;&quot; characters.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Person shown</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Enter a &quot;caption&quot; describing the who, what, and why of what is happening in this region.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>The Image Region Structure includes optionally any metadata property which is related to the region.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>New rectangle</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>New circle</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>New point</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>New polygon</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Delete region</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Image &amp;Regions</source>
+        <translation></translation>
     </message>
 </context>
 <context>
