3279907b8f3654e6c0f6c49810723032dca58d4b
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2023-05-15 16:24:30 +0100

    Don't truncate timestamp subseconds if non-zero

--- src/photini/types.py
+++ src/photini/types.py
@@ -297,7 +297,7 @@ class MD_DateTime(MD_Dict):
             precision = self['precision']
         fmt = ''.join(self._fmt_elements[:precision])
         datetime_string = self['datetime'].strftime(fmt)
-        if precision > 6:
+        if precision > 6 and datetime_string[-3:] == '000':
             # truncate subsecond to 3 digits
             datetime_string = datetime_string[:-3]
         if precision > 3 and time_zone and self['tz_offset'] is not None:
