be476c9968a6d74ec2f4084d157c71af05d68db0
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2023-05-16 12:48:03 +0100

    Add number units (mm, m, etc) to translations

--- src/photini/photinimap.py
+++ src/photini/photinimap.py
@@ -223,7 +223,8 @@ class PhotiniMap(QtWidgets.QWidget):
         label.setAlignment(Qt.AlignmentFlag.AlignRight)
         left_side.addWidget(label, 1, 0)
         self.widgets['alt'] = DoubleSpinBox('exif:GPSAltitude')
-        self.widgets['alt'].set_suffix(' m')
+        self.widgets['alt'].set_suffix(
+            translate('PhotiniMap', ' m', 'metres altitude'))
         self.widgets['alt'].new_value.connect(self.new_value)
         left_side.addWidget(self.widgets['alt'], 1, 1)
         if hasattr(self.geocoder, 'get_altitude'):
