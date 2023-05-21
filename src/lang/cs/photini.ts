e6587d5069f44db577fa4b0288536e4dcc5cb87d
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2023-05-21 10:54:18 +0100

    Tweak name of image regions tab

--- src/lang/cs/photini.ts
+++ src/lang/cs/photini.ts
@@ -1556,7 +1556,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Image &amp;Regions</source>
+        <source>Image &amp;regions</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
