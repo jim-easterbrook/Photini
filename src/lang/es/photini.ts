5174df12772e07c57f15d8b1459854d0f59c547c
Jim Easterbrook <github@jim-easterbrook.me.uk>
2024-01-12 08:29:24 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (439 of 439 strings)
    
    Translation: Photini/GUI
    Translate-URL: https://hosted.weblate.org/projects/photini/gui/es/

--- src/lang/es/photini.ts
+++ src/lang/es/photini.ts
@@ -842,7 +842,7 @@
     <name>LatLongDisplay</name>
     <message>
         <source>Lat, long</source>
-        <translation>Latitud longitud</translation>
+        <translation>Latitud longitud - this is too long, please see screenshots for available space</translation>
     </message>
     <message>
         <source>Latitude and longitude (in degrees) as two decimal numbers separated by a space.</source>
