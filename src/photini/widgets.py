jim-easterbrook/Photini

commit 76fca34e947922146c08396c0f6afc80208b369c

Fix problem with <multiple> for empty values

src/photini/widgets.py
@@ -52,9 +52,7 @@ class WidgetMixin(object):
         self.setEnabled(True)
         choices = []
         for value_dict in values:
-            value = None
-            if self._key in value_dict:
-                value = value_dict[self._key]
+            value = value_dict.get(self._key) or None
             if value not in choices:
                 choices.append(value)
         if len(choices) > 1:

commit 601669f8c93c54876a1d574b3a06e7f5163de32c

Fix bug introduced in 76fca34
set_value_list can be called with a list of metadata objects as well as
with a list of metadata values.

src/photini/widgets.py
@@ -52,7 +52,9 @@ class WidgetMixin(object):
         self.setEnabled(True)
         choices = []
         for value_dict in values:
-            value = value_dict.get(self._key) or None
+            value = None
+            if self._key in value_dict:
+                value = value_dict[self._key] or None
             if value not in choices:
                 choices.append(value)
         if len(choices) > 1: